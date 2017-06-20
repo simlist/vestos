36b0dfa4ab3479edfdebafee95f151bbe4b8d293
Meir S. List <simlist@gmail.com>
2017-06-20 08:34:00 -0400

    Began setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import io
+
+setup(name='vestos',
+      version='0.0.0dev',
+	  author='MS List',
+	  author_email='simlist@gmail.com',
+	  packages='vestos',
+	  license='MIT',
+	  description='',
+	  )
+
