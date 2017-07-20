3f46a0bdc5db71fbc7b989a0dd7778795003239f
Meir S. List <simlist@gmail.com>
2017-07-20 19:59:55 -0400

    Added description to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import io
 
 setup(name='vestos',
       version='0.0.0dev',
-	  author='MS List',
-	  author_email='simlist@gmail.com',
-	  packages='vestos',
-	  license='MIT',
-	  description='',
-	  )
+      author='MS List',
+      author_email='simlist@gmail.com',
+      packages=['vestos', 'pyluach'],
+      license='MIT',
+      description='Package for calculating vestos',
+     )
 
